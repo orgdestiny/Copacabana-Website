da373285b7aff504e2b24bf0cf8affec3a9b8f0f
argondc <90789517+argondc@users.noreply.github.com>
2023-01-11 18:40:47 -0300

    feat: section servers, social, about

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,8 @@
+const timeReveal = 500;
+
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('#serverSection', {delay: timeReveal});
+ScrollReveal().reveal('#serversSection', {delay: timeReveal});
+ScrollReveal().reveal('#socialTitleSection', {delay: timeReveal});
+ScrollReveal().reveal('#socialSection', {delay: timeReveal});
+ScrollReveal().reveal('#footerSection', {delay: timeReveal});
